de3aef5385bc5826f6e99ebb9e89ba21d2d67e7e
NM1 <canilmmc@gmail.com>
2024-09-21 00:40:54 -0300

    1.0.2
    Removed old commented code;
    Remade the Logo;
    Added manifest.json and README.md to the Github Repo;
    Added CHANGELOG.md;
    Actually put the current version and right name on the dll.

--- Plugin.cs
+++ Plugin.cs
@@ -10,8 +10,8 @@ namespace ArFe_ChaosMod
     {
         // MOD Info
         public const string modGUID = "ArrFel.OnlyGoldMod";
-        public const string modNAME = "Arroz's Only Gold Mod";
-        public const string modVER = "1.0.0";
+        public const string modNAME = "Only Gold Mod";
+        public const string modVER = "1.0.2";
         public const string modBY = "Mewings da Galaxia";
 
         private readonly Harmony harmony = new Harmony(modGUID);
@@ -36,54 +36,4 @@ namespace ArFe_ChaosMod
             mls.LogInfo("Patched Scripts.");
         }
     }
-
-    /*
-    public class Mono : MonoBehaviour
-    {
-        SelectableLevel[] SelectableLevels = GameObject.FindObjectsOfType<SelectableLevel>();
-        SpawnableItemWithRarity GoldDefault;
-        public GameObject goldAssingment;
-
-        // Set Up Variables
-        bool setup_gold;
-        bool setup_levels;
-        void Awake() 
-        {
-            setup_gold = false;
-            setup_levels = false;
-            goldAssingment = GameObject.Find("GoldBar");
-        }
-        void Update()
-        {
-            goldAssingment = GameObject.Find("GoldBar");
-            if (goldAssingment) { Main.inst.mls.LogInfo("FUCK"); }
-
-            // SETUP GOLDEN BARS
-            /*var GoldBar = GameObject.Find("GoldBar");
-            if (GoldBar && !setup_gold)
-            {
-                GoldDefault.rarity = 100;
-                GoldDefault.spawnableItem = GoldBar.GetComponent<Item>();
-                setup_gold = true;
-                Main.inst.mls.LogInfo("Setted Up Gold Object");
-            }
-            else if (!GoldBar && setup_gold) { setup_gold = false; }
-
-            // SETUP LEVELS
-            var DineLevel = GameObject.Find("DineLevel");
-            if (DineLevel != null && !setup_levels && setup_gold)
-            {
-                SelectableLevels = GameObject.FindObjectsOfType<SelectableLevel>();
-                for (int _i = 0; _i < SelectableLevels.Length; _i += 1)
-                {
-                    SelectableLevels[_i].spawnableScrap.Clear();
-                    SelectableLevels[_i].spawnableScrap.Add(GoldDefault);
-                }
-                setup_levels = true;
-                Main.inst.mls.LogInfo("Setted Up Levels");
-            }
-            else if (DineLevel == null && setup_levels) { setup_levels = false; }
-        }
-    }
-    */
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+# Arroz's Gold Only Mod
+This is a Lethal Company mod where's everything is golden bars.
